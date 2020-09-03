97c2a3a050b365cc53583a827ab8aa846be8d463
juancortelezzi <juanbicortelezzi@gmail.com>
2020-09-03 19:23:14 -0300

    added add to cart and plus and minus button to cards and respective functions

--- components/card.tsx
+++ components/card.tsx
@@ -1,28 +1,63 @@
+import { useState } from "react";
+
 export default function Card(props: {
   product_data: { title: string; description: string; image: string; value: number };
 }) {
+  const [count, setCount] = useState(0);
+
   const { product_data } = props;
   const { title, description, value, image } = product_data;
 
-  const handleClick = (e: React.MouseEvent) => {
+  const handlePlus = (e: React.MouseEvent) => {
     e.preventDefault();
-    console.log("clicked");
+    setCount(count + 1);
+  };
+
+  const handleMinus = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (count <= 0) return;
+    setCount(count - 1);
+  };
+
+  const handleCart = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (count <= 0) return;
+    setCount(0);
+    console.log(`${title}, ${count}, $${count * value}`);
   };
 
   return (
-    <a
-      href="#"
-      className="group block h-auto w-full max-w-sm rounded shadow-lg m-4 flex-grow-0"
-      onClick={handleClick}
-    >
-      <img className="w-full h-full max-h-3/4 object-cover" src={image} alt={title} />
-      <div className="w-full max-h-1/4 h-48 px-6 py-4 bg-gray-300">
-        <div className="flex flex-row flex-no-wrap justify-between align-center mb-3">
+    <div className="group block max-w-sm rounded shadow-lg m-4 flex-grow-1">
+      <img className="w-full h-128 object-cover" src={image} alt={title} />
+
+      <div className="h-56 px-6 py-4 bg-gray-300">
+        <div className="h-1/4 flex flex-row flex-no-wrap justify-between align-center">
           <h2 className="font-bold text-2xl">{title}</h2>
           <p className="font-bold text-xl text-gray-800">${value}</p>
         </div>
-        <p className="text-gray-700 text-base">{description}</p>
+        <div className="h-3/4 flex flex-col justify-between align-center">
+          <p className="text-gray-700 text-base overflow-hidden">{description}</p>
+          <div className="flex items-center justify-around">
+            <div className="grid grid-cols-3 divide-x divide-blue-700">
+              <button className="text-lg text-center font-medium px-4" onClick={handleMinus}>
+                -
+              </button>
+
+              <span className="text-lg text-center font-medium px-4">{count}</span>
+
+              <button className="text-lg text-center font-medium px-4" onClick={handlePlus}>
+                +
+              </button>
+            </div>
+            <button
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              onClick={handleCart}
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
       </div>
-    </a>
+    </div>
   );
 }
